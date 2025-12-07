0d3aa774e9c8e15889cc9a01008f050d29638147
Sanjay Kumar <insanjay.work@gmail.com>
2025-12-08 00:52:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,9 +70,9 @@ else:
         show_llm_chat_page(db)
 
     # # --- THIS IS THE FIX ---
-    # # We add a unique key to the logout button to prevent the duplicate ID error.
-    # if st.sidebar.button("Logout", key="main_logout_button"):
-    #     for key in st.session_state.keys():
-    #         del st.session_state[key]
+    # We add a unique key to the logout button to prevent the duplicate ID error.
+    if st.sidebar.button("Logout", key="main_logout_button"):
+        for key in st.session_state.keys():
+            del st.session_state[key]
         st.rerun()
 
